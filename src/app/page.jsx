0523f9974233f020d48ddc5e581ab7809786f82a
jesusrpd <jesusrodrigo881@gmail.com>
2023-06-22 10:24:30 -0600

    section home

---
+++ src/app/page.jsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="w-full h-screen flex flex-col justify-start items-center pt-5">
+      <Image src="/img/desktop.png" alt='img organize' width={300} height={300}/>
+      <div className='text-center mt-20'>
+        <h1 className='font-bold text-5xl'>ORGANIZE</h1>
+        <p className='my-7 font-medium'>La mejor forma de organizar tu vida y encontrar el orden que necesitas.</p>
+        <button className='bg-purple text-white px-7 py-1 font-bold rounded-xl shadow-button hover:bg-white hover:text-purple transition-all'>Comenzar</button>
+      </div>
+    </main>
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,20 @@ module.exports = {
   ],
   theme: {
     extend: {
+      backgroundColor:{
+        'purple': '#C891A2'
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      boxShadow:{
+        'button': '4px 4px 20px 0px rgba(0, 0, 0, 0.15), -4px -4px 20px 0px rgba(0, 0, 0, 0.08) inset, -4px -4px 20px 0px rgba(0, 0, 0, 0.15)'
+      },
+      colors:{
+        'purple': '#C891A2'
+      }
     },
   },
   plugins: [],
